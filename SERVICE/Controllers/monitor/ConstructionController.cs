ae64af15a81b8f7c2b42fc3d9bed74347c722d42
沐姜子 <@DESKTOP-B5M3HC1>
2022-07-01 17:26:56 +0800

    1.add constructionphoto

--- SERVICE/Controllers/monitor/ConstructionController.cs
+++ SERVICE/Controllers/monitor/ConstructionController.cs
@@ -18,6 +18,13 @@ namespace SERVICE
         private static Logger logger = Logger.CreateLogger(typeof(ConstructionController));
         private static string pgsqlConnection = ConfigurationManager.ConnectionStrings["postgresql"].ConnectionString.ToString();
 
+
+        /// <summary>
+        /// 施工信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="cookie"></param>
+        /// <returns></returns>
         [HttpGet]
         public string GetConstructionInfo(int id, string cookie)
         {
@@ -36,6 +43,58 @@ namespace SERVICE
                 }
             }
             return string.Empty;
+        }
+
+        /// <summary>
+        /// 施工影像集
+        /// </summary>
+        /// <param name="projectid"></param>
+        /// <param name="drasterid"></param>
+        /// <param name="monitorid"></param>
+        /// <param name="cookie"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public string GetConstructionPhoto(string projectid, string drasterid, string monitorid, string cookie)
+        {
+            string sql = "SELECT * FROM monitor_const_photo WHERE project_id ={0}";
+            if (!string.IsNullOrEmpty(drasterid))
+            {
+                sql = sql + " and drasterid = " + SQLHelper.UpdateString(drasterid);
+            }
+            if (!string.IsNullOrEmpty(monitorid))
+            {
+                sql = sql + " and monitorid = " + SQLHelper.UpdateString(monitorid);
+            }
+
+            sql = sql + "ORDER BY type_id ";
+            string data = PostgresqlHelper.QueryData(pgsqlConnection, string.Format(sql, SQLHelper.UpdateString(projectid)));
+            if (!string.IsNullOrEmpty(data))
+            {
+                List<ConstructionPhoto> constPhotoList = new List<ConstructionPhoto>();
+
+                string[] rows = data.Split(new char[] { COM.ConstHelper.rowSplit });
+                for (int i = 0; i < rows.Length; i++)
+                {
+                    ConstructionPhoto constPhoto = ParseContruction.ParseConstructionPhoto(rows[i]);       //ParseMapProjectWarningInfo(rows[i]);
+                    if (constPhoto != null)
+                    {
+                        constPhotoList.Add(constPhoto);
+                    }
+                }
+
+                if (constPhotoList.Count > 0)
+                {
+                    return JsonHelper.ToJson(constPhotoList);
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
     }
 }
